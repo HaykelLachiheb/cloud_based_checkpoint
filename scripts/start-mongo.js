f8dc418a37780dce283e85ae146fbcd211ac8dd4
HaykelLachiheb <your-email@example.com>
2026-07-02 22:18:46 +0100

    Cleanup: remove temp files and update gitignore

--- scripts/start-mongo.js
+++
@@ -1,9 +0,0 @@
-const { MongoMemoryServer } = require('mongodb-memory-server');
-
-async function startMongo() {
-  const mongod = await MongoMemoryServer.create();
-  const uri = mongod.getUri();
-  console.log(uri);
-}
-
-startMongo().catch(console.error);
